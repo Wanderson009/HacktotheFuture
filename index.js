79bfca1bd35086a3ad335ddbc8e4d3ae8201048c
Santos Wanderson O <wanderson.o.santos@outlook.com>
2021-09-03 14:11:20 -0300

    Initial Version :rocket:

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http")
+
+const server = http.createServer( (req,res) => {
+    res.statusCode = 200
+    res.setHeader = ('Content-Type', 'text/plain')
+    res.end('Hack to the Future')
+} )
+
+server.listen(3000,"127.0.0.1",( )=> console.log("Server is running: http://127.0.0.1:3000"))
